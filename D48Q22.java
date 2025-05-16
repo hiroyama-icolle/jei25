5f99018d790132ec2d9930f94c054c1d550d8d31
hiroyama-icolle <hiroyama@std.it-college.ac.jp>
2025-05-16 12:39:42 +0900

    課題

---
+++ D48Q22.java
@@ -0,0 +1,14 @@
+enum Connection {OFFLINE, ONLINE}
+
+public class D48Q22 {
+    public static void main(String[] args){
+        Connection s = Connection.OFFLINE;
+        if(s == Connection.OFFLINE){
+            s = Connection.ONLINE;
+        }
+        System.out.println(s);
+    }
+}
+/*
+正しくコンパイル、実行できるようにコードを修正した
+ */
---
+++ D48Q27.java
@@ -0,0 +1,13 @@
+public class D48Q27 {
+    public static void main(String[] args){
+        int i = 0;
+        while(i <= 6){
+            System.out.print(i);
+            i++;
+        }
+    }
+}
+/*
+問題だと「0」が繰り返し表示されてしまいますが、
+これを「0123456」と出力されるように修正した
+ */
