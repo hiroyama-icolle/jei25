23aa9f1b0131fdc07ded0c8735d327ea019ff101
hiroyama-icolle <hiroyama@std.it-college.ac.jp>
2025-05-23 12:31:56 +0900

    課題

---
+++ D49Q21.java
@@ -0,0 +1,13 @@
+public class D49Q21 {
+    public static void main(String[] args){
+        int n1 = 0100; //64
+        int n2 = 0x10; //16
+
+        System.out.println(n1 + n2);
+    }
+}
+/*
+コードの修正をしたいのですが、
+コンパイル及び実行され、「80」が表示されるようにコードを修正
+してみてください(64+16=80)
+ */
